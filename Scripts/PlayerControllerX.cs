014cf3a4ae7b2ec6253f4bde8d0da2e0caf243d7
KillerMaker <emmanuelmtr1234@gmail.com>
2021-09-20 16:11:57 -0400

    first commit

---
+++ Scripts/PlayerControllerX.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    public float speedMultiplier=2;
+    private float gravityModifier = 1;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up*speedMultiplier,ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+            Invoke(nameof(restartScene), 2f);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+    public void restartScene()
+    {
+        SceneManager.LoadScene("Challenge 3");
+    }
+
+}
---
+++ Scripts/PlayerDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem explosion;
+    private PlayerControllerX playerController;
+
+    private void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            explosion.Play();
+            playerController.gameOver = true;
+            Invoke(nameof(playerController.restartScene), 2f);
+        }
+
+    }
+
+}
